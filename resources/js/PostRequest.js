9fe50ad1eb47d8e390c7edbbbb39dec752d38a25
Ronald Marmol <ronaldmarmol003@hotmail.es>
2017-12-11 20:37:30 -0400

    poniendo a guardar los usuarios en la local storage

--- resources/js/PostRequest.js
+++ resources/js/PostRequest.js
@@ -20,7 +20,7 @@ function cargarPost(root) {
                 "<span class='glyphicon glyphicon-user '></span>Ronald Marmol @hotmail.com</a>" +
                 "</div>" +
                 "<div class='col-sm-2'>" +
-                "<button class=" + (existe ? 'glyphicon-star' : ' glyphicon-star-empty') + ""btn glyphicon  post_button" data-post_id='" + postData.id + "'></button>" +
+                "<button class='btn glyphicon " + (existe ? 'glyphicon-star' : 'glyphicon-star-empty') + " post_button' data-postid='" + postData.id + "'></button>" +
                 "</div>" +
                 "</div>" +
                 "<div class='row'>" +
@@ -31,7 +31,7 @@ function cargarPost(root) {
             console.log(post);
             $('#post').append(post);
         });
-        $('.post_boton').click(function () {
+        $('.post_button').click(function () {
             let postId = $(this).data('postid');
             let existe = agregarPostFavorito(postId);
 
@@ -56,14 +56,57 @@ function cargarUsuarios(root) {
         method: 'GET'
     }).then(function (data) {
         let localStorage = window.localStorage;
-        let users = {};
+        let users = [];
         let dbUsers = localStorage.getItem('users');
         if (dbUsers != null) {
+            console.log("La db local no esta nula");
             users = JSON.parse(dbUsers);
+            for (let j = 0; j < users.length; j++) {
+                console.log(users);
+                addUsersToDb(users)
+            }
+        } else {
+            console.log("La db local ESTA NULA Y RECORRO PARA AGREGAR");
+            if (data.length > 1) {
+                $.each(data, function (i, userData) {
+                    let user = new User();
+                    user.id = userData.id;
+                    user.name = userData.name;
+                    user.username = userData.username;
+                    user.email = userData.email;
+                    user.phone = userData.phone;
+                    user.website = userData.website;
+                    users.push(user);
+                });
+            }
+            console.log(users.length);
+            for (let user of users) {
+                if (user instanceof User) {
+                    addUsersToDb(users)
+                }
+            }
         }
-        console.log(data)
     });
+}
 
+function addUsersToDb(userResponse) {
+    let lsUsers = window.localStorage;
+    let userToUpdate = [];
+    let dbUsers = lsUsers.getItem('users');
+    if (dbUsers != null) {
+        userToUpdate = JSON.parse(dbUsers);
+        for (let i = 0; i < userToUpdate; i++) {
+            if (!userResponse.id in userToUpdate[i].id) {
+                delete  userToUpdate[i];
+            } else {
+                userToUpdate[i].push(userResponse)
+            }
+        }
+    } else {
+        userToUpdate.push(userResponse)
+    }
+
+    lsUsers.setItem('users', JSON.stringify(userToUpdate));
 }
 
 function agregarPostFavorito(postId) {
@@ -90,3 +133,17 @@ $(document).ready(function () {
     cargarUsuarios(root);
     cargarPost(root);
 });
+
+var User = function () {
+    let self = this;
+    self.id;
+    self.name;
+    self.username;
+    self.email;
+    self.phone;
+    self.website;
+
+    function toString() {
+        self.id + " " + self.name + " " + self.email;
+    }
+};
